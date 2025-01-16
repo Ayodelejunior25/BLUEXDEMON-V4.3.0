027549458d768395f1a57bb6252e1f5183edfe89
Ayodelejunior25 <ayodelebonnke@gmail.com>
2025-01-16 14:52:29 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 //Input number for pair code
-global.pairingNumber = "2348117485055"
+global.pairingNumber = "2349136939948"
 
 
 
@@ -21,9 +21,9 @@ global.alwaysonline = true
 global.autoswview = false
 global.public = true
 global.autoreact = false
-global.antidelete = false
-global.autotyping = false
-global.autoBio = true
+global.antidelete = true
+global.autotyping = true
+global.autoBio = false
 global.antiSpam = true
 global.sign = "𓃵"
 // ☞ ➻ ➽ ☛ 〆 
